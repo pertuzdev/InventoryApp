5db0c61771416badb8b80f0dc018764dd51dfccc
pertuzdev <antonio.pertuz01@gmail.com>
2022-01-04 22:36:35 -0600

    Refactor and organize code

---
+++ src/components/BottomSheet.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import {Colors} from '../styles/Colors';
+
+export default function BottomSheet({
+  refRBSheet,
+  takePhoto,
+  chosePhotoFromGallery,
+}) {
+  return (
+    <RBSheet
+      ref={refRBSheet}
+      height={170}
+      animationType={'fade'}
+      closeOnDragDown={true}
+      closeOnPressMask={true}
+      customStyles={{
+        wrapper: {
+          backgroundColor: 'rgba(0,0,0,0.5)',
+        },
+        draggableIcon: {
+          backgroundColor: '#000',
+        },
+      }}>
+      <View style={styles.sheetContainer}>
+        <Text style={styles.sheetTitle}>Foto del producto</Text>
+        <View style={styles.sheetWrapper}>
+          <Pressable
+            style={styles.sheetBtn}
+            android_ripple={{color: Colors.mediumGray}}
+            onPress={takePhoto}>
+            <View style={styles.sheetBtnWrapp}>
+              <Image
+                style={styles.sheetOptIcon}
+                source={require('../assets/icons/ic_camera.png')}
+              />
+            </View>
+            <Text style={styles.sheetIconTxt}>Cámara</Text>
+          </Pressable>
+          <Pressable
+            style={styles.sheetBtn}
+            android_ripple={{color: Colors.mediumGray}}
+            onPress={chosePhotoFromGallery}>
+            <View style={styles.sheetBtnWrapp}>
+              <Image
+                style={styles.sheetOptIcon}
+                source={require('../assets/icons/ic_gallery.png')}
+              />
+            </View>
+            <Text style={styles.sheetIconTxt}>Galería</Text>
+          </Pressable>
+        </View>
+      </View>
+    </RBSheet>
+  );
+}
+
+const styles = StyleSheet.create({
+  sheetContainer: {
+    marginLeft: 8,
+  },
+  sheetTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 8,
+    marginLeft: 16,
+  },
+  sheetWrapper: {
+    flexDirection: 'row',
+    marginVertical: 0,
+    marginLeft: 8,
+    //backgroundColor: 'blue',
+  },
+  sheetBtn: {
+    marginRight: 24,
+    padding: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sheetBtnWrapp: {
+    padding: 12,
+    borderWidth: 1,
+    borderRadius: 100,
+  },
+  sheetOptIcon: {
+    width: 25,
+    height: 25,
+  },
+  sheetIconTxt: {
+    marginTop: 4,
+  },
+});
--- src/screens/CreateItemScreen/CreateItemScreen.js
+++ src/screens/CreateItemScreen/CreateItemScreen.js
@@ -1,51 +1,27 @@
-import React, {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
-import {
-  Image,
-  Pressable,
-  Keyboard,
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-  BackHandler,
-} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Keyboard, StyleSheet, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 
 import {useForm} from 'react-hook-form';
 
-import RBSheet from 'react-native-raw-bottom-sheet';
-
 import firestore from '@react-native-firebase/firestore';
 
-import storage from '@react-native-firebase/storage';
-
-import ImagePicker from 'react-native-image-crop-picker';
-
 import {addItem} from '../../services/firestore/addItem';
-import Button from '../../components/Button/Button';
-import TextButton from '../../components/Button/TextButton';
 import Form from '../../components/form/Form';
 
-import {Colors} from '../../styles/Colors';
 import ActivityIndicator from '../../components/ActivityIndicator';
 import {uploadFile} from '../../services/cloudStorage/uploadFile';
 import {useAlertOnGoBack} from '../../hooks/useAlertOnGoBack';
 import {alertOnGoBack} from '../../helpers/alertOnGoBack';
 import BottomOptions from '../../components/BottomOptions';
 import useImagePick from '../../hooks/useImagePick';
+import BottomSheet from '../../components/BottomSheet';
+import SelectImage from '../../components/SelectImage';
 
 export default function CreateItemScreen({navigation}) {
   const dateCaptured = new Date();
 
   const refRBSheet = useRef();
-
-  //const [image, setImage] = useState(null);
 
   const [loading, setLoading] = useState(false);
 
@@ -103,9 +79,7 @@ export default function CreateItemScreen({navigation}) {
 
   const hasUnsavedChanges = () => {
     const {code, name, cost, description} = control._formValues;
-
     const validation = code || name || cost || description ? true : false;
-
     return validation ? true : false;
   };
 
@@ -120,26 +94,7 @@ export default function CreateItemScreen({navigation}) {
       <View style={styles.container}>
         <ScrollView style={styles.scrollCont}>
           <View style={styles.head}>
-            {/* <Text style={TextStyles.title}>Zapatos Rojos</Text> */}
-            <Pressable
-              style={styles.imgCont}
-              onPress={openSheetBottom}
-              android_ripple={{color: Colors.darkGray}}>
-              {image ? (
-                <Image source={{uri: image}} style={styles.img} />
-              ) : (
-                <Image
-                  source={require('../../assets/icons/ic_camera.png')}
-                  style={styles.splashImg}
-                />
-              )}
-              <Pressable
-                style={styles.editBtn}
-                onPress={openSheetBottom}
-                android_ripple={{color: Colors.darkGray}}>
-                <Image source={require('../../assets/icons/ic_pencil.png')} />
-              </Pressable>
-            </Pressable>
+            <SelectImage openSheetBottom={openSheetBottom} image={image} />
           </View>
           <Form control={control} errors={errors} dateCaptured={dateCaptured} />
         </ScrollView>
@@ -148,50 +103,11 @@ export default function CreateItemScreen({navigation}) {
           handleSavePress={handleSubmit(handleSave)}
           handleSavePressOut={() => Keyboard.dismiss()}
         />
-        <RBSheet
-          ref={refRBSheet}
-          height={170}
-          animationType={'fade'}
-          closeOnDragDown={true}
-          closeOnPressMask={true}
-          customStyles={{
-            wrapper: {
-              backgroundColor: 'rgba(0,0,0,0.5)',
-            },
-            draggableIcon: {
-              backgroundColor: '#000',
-            },
-          }}>
-          <View style={styles.sheetContainer}>
-            <Text style={styles.sheetTitle}>Foto del producto</Text>
-            <View style={styles.sheetWrapper}>
-              <Pressable
-                style={styles.sheetBtn}
-                android_ripple={{color: Colors.mediumGray}}
-                onPress={takePhoto}>
-                <View style={styles.sheetBtnWrapp}>
-                  <Image
-                    style={styles.sheetOptIcon}
-                    source={require('../../assets/icons/ic_camera.png')}
-                  />
-                </View>
-                <Text style={styles.sheetIconTxt}>Cámara</Text>
-              </Pressable>
-              <Pressable
-                style={styles.sheetBtn}
-                android_ripple={{color: Colors.mediumGray}}
-                onPress={chosePhotoFromGallery}>
-                <View style={styles.sheetBtnWrapp}>
-                  <Image
-                    style={styles.sheetOptIcon}
-                    source={require('../../assets/icons/ic_gallery.png')}
-                  />
-                </View>
-                <Text style={styles.sheetIconTxt}>Galería</Text>
-              </Pressable>
-            </View>
-          </View>
-        </RBSheet>
+        <BottomSheet
+          refRBSheet={refRBSheet}
+          takePhoto={takePhoto}
+          chosePhotoFromGallery={chosePhotoFromGallery}
+        />
       </View>
     </ActivityIndicator>
   );
@@ -202,82 +118,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollCont: {},
-  head: {
-    alignItems: 'center',
-    marginTop: 20,
-    marginBottom: 16,
-  },
-  imgCont: {
-    width: 200,
-    height: 200,
-    backgroundColor: Colors.gray,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 15,
-  },
-  img: {
-    //flex: 1,
-    //resizeMode: 'contain',
-    width: 200,
-    height: 200,
-    borderRadius: 15,
-    //backgroundColor: 'blue',
-  },
-  splashImg: {
-    width: 100,
-    height: 100,
-    resizeMode: 'contain',
-    //backgroundColor: 'blue',
-  },
   options: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 16,
     marginBottom: 16,
     marginHorizontal: 20,
-  },
-  editBtn: {
-    //alignSelf: 'flex-end',
-    position: 'absolute',
-    bottom: -10,
-    right: -10,
-    //margin: -16,
-    backgroundColor: Colors.primaryBlue,
-    padding: 8,
-    borderWidth: 1,
-    borderRadius: 100,
-  },
-  sheetContainer: {
-    marginLeft: 8,
-  },
-  sheetTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginVertical: 8,
-    marginLeft: 16,
-  },
-  sheetWrapper: {
-    flexDirection: 'row',
-    marginVertical: 0,
-    marginLeft: 8,
-    //backgroundColor: 'blue',
-  },
-  sheetBtn: {
-    marginRight: 24,
-    padding: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  sheetBtnWrapp: {
-    padding: 12,
-    borderWidth: 1,
-    borderRadius: 100,
-  },
-  sheetOptIcon: {
-    width: 25,
-    height: 25,
-  },
-  sheetIconTxt: {
-    marginTop: 4,
   },
 });
